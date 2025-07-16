29354d74c3d53999de642543e137945caba430a2
ShashiRanjan42 <101126472+ShashiRanjan42@users.noreply.github.com>
2025-07-16 12:24:52 +0530

    Update server.js

--- server.js
+++ server.js
@@ -15,7 +15,7 @@ app.use(express.urlencoded({ extended: true }))
 
 //Cors
 app.use(cors({
-  origin: ["http://localhost:3000", "https://grovoo.vercel.app"],
+  origin: ["http://localhost:3000", "*"],
   methods: ["GET", "POST", "PUT", "DELETE", "PATCH", "OPTIONS"],
   credentials: true,
 }));
